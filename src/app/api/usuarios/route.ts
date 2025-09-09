f301888c38625a2280adc9d65fc5461841b9e65e
erosMariano <erosmariano1@gmail.com>
2025-09-09 19:06:30 -0300

    feat(DB): Adição de lista de usuários

--- src/app/api/usuarios/route.ts
+++ src/app/api/usuarios/route.ts
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 export async function GET(request: Request) {
-  return NextResponse.json({ message: "Listar todos os usuários" });
+  const users = await prisma.usuario.findMany();
+
+  return NextResponse.json(users);
 }
 
 export async function POST(request: Request) {
